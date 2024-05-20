8c640fa61defc07341bde4fccd523dfed6169684
Varun Babu <varun.babu@nestgroup.net>
2024-05-20 10:45:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app2';
+  receivedData: string = '';
+
+
+  // constructor(private router: Router) {
+  //   const navigation = this.router.getCurrentNavigation();
+  //   console.log('Navigation:', navigation);
+  //   if (navigation && navigation.extras && navigation.extras.state) {
+  //     console.log('State:', navigation.extras.state);
+  //     this.receivedData = navigation.extras.state['data'];
+  //   }
+  // }
+
+  constructor(private route: ActivatedRoute,private router: Router) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.receivedData = params['data'];
+      console.log("Data received in App2:", this.receivedData);
+    });
+  }
+
+  navigateBackToApp1() {
+    this.router.navigateByUrl('/app1');
+  }
+
+  
 }
